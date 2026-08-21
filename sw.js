b39b0ee9293d3e95aae4f818eb3ba005cf91bc33
majjoul <majhoulxks12@gmail.com>
2026-08-21 19:15:28 +0200

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'muminun-v1';
+const urlsToCache = ['./', './index.html'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((res) => {
+            return res || fetch(event.request);
+        })
+    );
+});
+
